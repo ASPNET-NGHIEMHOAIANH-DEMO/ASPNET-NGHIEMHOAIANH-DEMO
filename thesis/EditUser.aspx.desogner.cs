5ff9785d9377c73258603fea41a2f6d23345de44
ASPNET-DK24TTC7-NGHIEMHOAIANH-DEMO <anhnh281003@tvu-onschool.edu.vn>
2026-04-26 11:58:58 +0700

    EditUser.aspx.desogner.cs

---
+++ thesis/EditUser.aspx.desogner.cs
@@ -0,0 +1,11 @@
+
+namespace web_audio_music
+{
+    public partial class EditUser {
+        protected global::System.Web.UI.WebControls.Label lblMsg;
+        protected global::System.Web.UI.WebControls.TextBox txtUser;
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+        protected global::System.Web.UI.WebControls.TextBox txtPass;
+        protected global::System.Web.UI.WebControls.Button btnSave;
+    }
+}

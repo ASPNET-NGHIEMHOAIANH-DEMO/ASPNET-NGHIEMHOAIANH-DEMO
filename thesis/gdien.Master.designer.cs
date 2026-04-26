ce9d15f5f76843ccfa1e5ae30e54ac39e1427b09
ASPNET-DK24TTC7-NGHIEMHOAIANH-DEMO <anhnh281003@tvu-onschool.edu.vn>
2026-04-26 11:53:51 +0700

    gdien.Master.designer.cs

---
+++ thesis/gdien.Master.designer.cs
@@ -0,0 +1,48 @@
+
+namespace web_audio_music
+{
+
+
+    public partial class gdien
+    {
+
+      
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+
+      
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+       
+        protected global::System.Web.UI.WebControls.TextBox txtGlobalSearch;
+
+       
+        protected global::System.Web.UI.WebControls.Button btnGlobalSearch;
+
+      
+        protected global::System.Web.UI.WebControls.Panel pnlGuest;
+
+    
+        protected global::System.Web.UI.WebControls.Panel pnlUser;
+
+      
+        protected global::System.Web.UI.WebControls.Label lblUsername;
+
+       
+        protected global::System.Web.UI.WebControls.HyperLink hlManage;
+
+      
+        protected global::System.Web.UI.WebControls.LinkButton lnkLogout;
+
+      
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl heroSection;
+
+      
+        protected global::System.Web.UI.WebControls.TextBox txtSearchBox;
+
+       
+        protected global::System.Web.UI.WebControls.Button btnSearchBox;
+
+       
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+    }
+}

a0532e10e1999886f0296361bf8f8f3ec1d96b7a
ASPNET-DK24TTC7-NGHIEMHOAIANH-WEBAUDIOMUSIC <anhnh281003@tvu-onschool.edu.vn>
2026-04-28 19:09:26 +0700

    Register.aspx.designer.cs

---
+++ thesis/Register.aspx.designer.cs
@@ -0,0 +1,12 @@
+
+namespace web_audio_music
+{
+    public partial class Register {
+        protected global::System.Web.UI.WebControls.Label lblMsg;
+        protected global::System.Web.UI.WebControls.TextBox txtUser;
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+        protected global::System.Web.UI.WebControls.TextBox txtPass;
+        protected global::System.Web.UI.WebControls.TextBox txtPass2;
+        protected global::System.Web.UI.WebControls.Button btnRegister;
+    }
+}
